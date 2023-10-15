593fe77325b0e4d59173139abc47c64fde24e414
Kunal Kumar <kumarkunalkarn@gmail.com>
2023-10-15 14:53:21 +0530

    Update CandidateController.java

--- VotingApp/src/main/java/com/controller/CandidateController.java
+++ VotingApp/src/main/java/com/controller/CandidateController.java
@@ -11,59 +11,48 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.model.Candidate;
-
 import com.service.CandidateService;
-
-
 
 @Controller
 public class CandidateController {
-	
-	@Autowired
-	private CandidateService cndServ;
-	
 
-	
-	@PostMapping("/addcandidate")
-	public String addCandidate(@RequestParam("candidate") String candidate, 
-			Principal p,Model model, HttpSession session)
-	{
-		String email = p.getName();
-		
-		if (cndServ.getCandByUser(email) != null) {
-					
-			session.setAttribute("msg", "Already Voted");
-		}
-		else
-		{
-			String candidate1 = "";
-			String candidate2 = "";
-			String candidate3 = "";
-			String candidate4 = "";
+    @Autowired
+    private CandidateService cndServ;
 
-			if (candidate.equals("candidate1"))
-				candidate1 = email;
-			else if (candidate.equals("candidate2"))
-				candidate2 = email;
-			else if (candidate.equals("candidate3"))
-				candidate3 = email;
-			else if (candidate.equals("candidate4"))
-				candidate4 = email;
+    @PostMapping("/addcandidate")
+    public String addCandidate(@RequestParam("candidate") String selectedCandidate,
+            Principal principal, Model model, HttpSession session) {
+        String email = principal.getName();
 
-			Candidate cnd = new Candidate();
-			cnd.setCandidate1(candidate1);
-			cnd.setCandidate2(candidate2);
-			cnd.setCandidate3(candidate3);
-			cnd.setCandidate4(candidate4);
+        if (cndServ.getCandidateByUser(email) != null) {
+            session.setAttribute("msg", "Already Voted");
+        } else {
+            Candidate cnd = cndServ.getCandidateByUser(email);
+            if (cnd == null) {
+                cnd = new Candidate();
+            }
 
-			cndServ.addCandidate(cnd);
-			
-			session.setAttribute("msg", "Successfully Voted...");
-		}
-		
-		return "redirect:user/";
-	}
-	
-	
+            switch (selectedCandidate) {
+                case "candidate1":
+                    cnd.setCandidate1(email);
+                    break;
+                case "candidate2":
+                    cnd.setCandidate2(email);
+                    break;
+                case "candidate3":
+                    cnd.setCandidate3(email);
+                    break;
+                case "candidate4":
+                    cnd.setCandidate4(email);
+                    break;
+                default:
+                    // Handle unexpected candidate value
+            }
 
+            cndServ.addCandidate(cnd);
+            session.setAttribute("msg", "Successfully Voted...");
+        }
+
+        return "redirect:user/";
+    }
 }
